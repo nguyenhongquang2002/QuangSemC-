074d77f85cab36b3b35147ea6b4e1781edaceabf
nguyenhongquang2002 <71063187+nguyenhongquang2002@users.noreply.github.com>
2021-11-09 10:07:32 +0700

    Nop Bai Thi C#

---
+++ HongQuangUWP/Applayout.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HongQuangUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Applayout : Page
+    {
+        public Applayout()
+        {
+            this.InitializeComponent();
+        }
+        private void MyNavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            MyNavView.IsBackEnabled = true;
+            MyNavView.IsBackButtonVisible = NavigationViewBackButtonVisible.Visible;
+
+            var tag = args.InvokedItemContainer.Tag.ToString();
+            switch (tag)
+            {
+                case "name":
+                    ContentPage.Navigate(typeof(Pages.AppContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+                case "seach":
+                    ContentPage.Navigate(typeof(Pages.SearchContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+            }
+        }
+
+        private void MyNavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentPage.CanGoBack)
+            {
+                ContentPage.GoBack();
+            }
+        }
+    }
+}
